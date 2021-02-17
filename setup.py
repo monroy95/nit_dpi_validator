f80870dcdfd05e2d41358a500a4f5d4343539625
monroy95 <m.monroyc22@gmail.com>
2021-02-16 18:37:34 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name = 'nit_dpi_validator',
     packages = ['nit_dpi_validator'], # this must be the same as the name above
     version = '0.1',
@@ -9,14 +12,7 @@ setup(
     author_email = 'm.monroyc22@hotmail.com',
     license='LGPL',
     url = 'https://github.com/monroy95/nit_dpi_validator', # use the URL to the github repo
-    download_url = 'https://github.com/monroy95/nit_dpi_validator/tags',
     keywords = ['dpi', 'nit', 'Validador NIT Guatemala', 'NIT Guatemala'],
-    classifiers = [
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License '
-        '(LGPL)',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    long_description=long_description,
+    python_requires='>=2.7',
 )
