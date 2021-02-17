937101ec77793bfded592f960bb99e420bf77b15
monroy95 <m.monroyc22@gmail.com>
2021-02-16 18:32:17 -0600

    Agregando guia uso

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nit_dpi_validator
 Validador de NIT, DPI Guatemala
+
+The project is hosted on GitHub. Contributions are welcome.
+
+ GitHub: [monroy95/nit_dpi_validator: Validador de NIT, DPI Guatemala (github.com)](https://github.com/monroy95/nit_dpi_validator)
+
+Instalación
+------------
+
+La forma fácil de instalar `nit_dpi_validator` es usando **pip**:
+
+    pip install nit_dpi_validator
+
+Uso
+-----
+
+    >>> from nit_dpi_validator import nit
+    >>> nit.nit_validator('xxxxxxxx-x')
+    True
--- nit_dpi_validator/nit.py
+++ nit_dpi_validator/nit.py
@@ -1,7 +1,7 @@
 import re
 
 
-def nit_validador(nit):
+def nit_validator(nit):
     """
     Validador de NIT Guatemala
 
@@ -10,17 +10,17 @@ def nit_validador(nit):
 
     Returns:
         bool: True/False
-    """    
+    """
     try:
         nit = str(nit)
         if not nit:
             return False
-        
+
         # Expresion regular, para validar si aplica el patron
         nit_reg_exp = re.compile('^[0-9]+(-?[0-9kK])?$')
         if not nit_reg_exp.match(nit):
             return False
-        
+
         # Elimacion guiones
         nit = nit.replace('-', '')
         # Digito verificador
@@ -29,7 +29,7 @@ def nit_validador(nit):
         dig_nit = nit[0:-1]
         # Conversion digito verificados a str y minuscula (en caso aplique)
         checker = str(last_position).lower()
-        
+
         factor_nit = len(dig_nit) + 1
         total = 0
 
@@ -40,7 +40,7 @@ def nit_validador(nit):
 
         modulus = (11 - (total % 11)) % 11
         computed_checker = "k" if modulus == 10 else str(modulus)
-        
+
         if checker == computed_checker:
             return True
         else:
